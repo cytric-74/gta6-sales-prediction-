8cf4aa50d0e6683ed5078843ee1342807ccb9c97
cytric-74 <rohansix677@gmail.com>
2025-05-12 10:48:00 +0530

    initial commits

--- webscrapping.py
+++ webscrapping.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-def scrape_news(query):   # main keyword used for searching in google search engine
+def scrape_news(query):   # query = main keyword used for searching in google search engine
     base_url = "https://news.google.com/search"
     search_url = f"{base_url}?q={query}"
     response = requests.get(search_url)
@@ -18,4 +18,4 @@ def scrape_news(query):   # main keyword used for searching in google search eng
     df = pd.DataFrame(articles)
     df.to_csv('data/news_articles.csv', index=False)
 
-scrape_news("GTA 6")
+scrape_news("GTA 6") # passing the value of query herew
