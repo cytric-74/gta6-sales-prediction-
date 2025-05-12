8172f96f0d20c92b3940dbb060454333c72843e6
cytric-74 <rohansix677@gmail.com>
2025-05-12 10:43:47 +0530

    commit for webscrapping

---
+++ webscrapping.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def scrape_news(query):
+    base_url = "https://news.google.com/search"
+    search_url = f"{base_url}?q={query}"
+    response = requests.get(search_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    articles = []
+    for a in soup.select('article h3'):
+        if a.a:
+            title = a.text
+            link = "https://news.google.com" + a.a['href'][1:]
+            articles.append({'title': title, 'link': link})
+    
+    df = pd.DataFrame(articles)
+    df.to_csv('data/news_articles.csv', index=False)
+
+scrape_news("GTA 6")
