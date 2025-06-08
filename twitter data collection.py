83974a8ca02d40c3326e555c432df684ff96e630
cytric-74 <rohansix677@gmail.com>
2025-06-08 19:49:39 +0530

    minor changes

--- twitter data collection.py
+++ twitter data collection.py
@@ -7,14 +7,13 @@ def fetch_twitter_data(query, max_results=50):
     headers = {"Authorization": f"Bearer {BEARER_TOKEN}"}
     url = f"https://api.twitter.com/2/tweets/search/recent?query={query}&tweet.fields=created_at,public_metrics&max_results={max_results}"
     response = requests.get(url, headers=headers)
-    tweets = response.json()['data']
-    
+    tweets = response.json().get('data', [])
+
     df = pd.DataFrame([{
         'text': tweet['text'],
         'created_at': tweet['created_at'],
         'likes': tweet['public_metrics']['like_count']
     } for tweet in tweets])
-    
-    df.to_csv('data/twitter_data.csv', index=False)
 
-fetch_twitter_data("GTA 6")
+    df.to_csv('data/twitter_data.csv', index=False)
+    return df
--- youtube data collection.py
+++ youtube data collection.py
@@ -1,25 +1,27 @@
 from googleapiclient.discovery import build
 import pandas as pd
 
-API_KEY = 'need to get a youtube api key asap'
-youtube = build('youtube', 'v3', developerKey=API_KEY)
+YOUTUBE_API_KEY = 'need to get a youtube api key asap'
 
-def get_youtube_comments(query):
-    request = youtube.search().list(q=query, part='snippet', type='video', maxResults=10)
-    videos = request.execute()
-    
-    video_ids = [item['id']['videoId'] for item in videos['items']]
+youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
+
+def fetch_youtube_comments(query):
+    search_response = youtube.search().list(
+        q=query, part='snippet', type='video', maxResults=5
+    ).execute()
+
+    video_ids = [item['id']['videoId'] for item in search_response['items']]
     all_comments = []
 
     for vid in video_ids:
-        comments = youtube.commentThreads().list(
+        comment_response = youtube.commentThreads().list(
             part='snippet', videoId=vid, textFormat='plainText', maxResults=50
         ).execute()
-        
-        for item in comments.get("items", []):
-            all_comments.append(item['snippet']['topLevelComment']['snippet']['textDisplay'])
-    
+
+        for item in comment_response.get("items", []):
+            comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+            all_comments.append(comment)
+
     df = pd.DataFrame({'comment': all_comments})
     df.to_csv('data/youtube_data.csv', index=False)
-
-get_youtube_comments("GTA 6 trailer")
+    return df
